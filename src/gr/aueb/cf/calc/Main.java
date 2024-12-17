125bc61c361da7448380ded8da462c86199299ea
ilias Barbitsiotis <hbarbitsiotis@windowslive.com>
2024-12-17 19:10:45 +0200

    mod method created

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -10,11 +10,15 @@ public class Main {
         return a + b;
     }
 
-    public static int sub (int a, int b) {
+    public static int sub(int a, int b) {
         return a - b;
     }
 
-    public static int div (int a , int b) {
+    public static int div(int a, int b) {
         return a / b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
     }
 }
